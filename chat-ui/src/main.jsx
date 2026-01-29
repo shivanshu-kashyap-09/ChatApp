d7a643a3a3d01717aac790bd239891e57b0d5ffb
shivanshu kashyap <kashyapshivanshu63@gmail.com>
2026-01-30 00:11:50 +0530

    Fix import path for ChatProvider component

--- chat-ui/src/main.jsx
+++ chat-ui/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router";
 // import AppRoutes from "./config/routes.jsx";
 import { Toaster } from "react-hot-toast";
-import { ChatProvider } from "./context/ChatContext.jsx";
+import { ChatProvider } from "./Context/ChatContext.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
